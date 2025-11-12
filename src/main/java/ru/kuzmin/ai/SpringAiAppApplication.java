f58f868a05ead0ca2ea8cf3d66812d8d1836f474
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2025-11-12 11:46:54 +0300

    MVP - работает только в коде, нет пока чатов и прочего

---
+++ src/main/java/ru/kuzmin/ai/SpringAiAppApplication.java
@@ -0,0 +1,23 @@
+package ru.kuzmin.ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringAiAppApplication {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringAiAppApplication.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Дай первую строчку Bohemian Rhapsody").call().content());
+    }
+
+}
